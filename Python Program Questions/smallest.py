8b05f9ca8c4d5b9eb943c3a6dff9a87f86fd1e49
TIRTHANKAR DEBNATH <55769445+tirthodebnath@users.noreply.github.com>
2023-04-25 02:11:04 +0530

    Create smallest.py

---
+++ Python Program Questions/smallest.py
@@ -0,0 +1,15 @@
+##Python Program to find Smallest number among three.
+
+a =[10,5,3]
+b=0
+
+def smallest_3():
+    for i in a:
+        if a[0]>a[1] and a[0]>a[2] :
+            print(a[0],"Biggest")
+        elif a[1]>a[0] and a[1]>a[2]:
+            print(a[1],"Biggest")
+        else:
+            print(a[2],"Biggest") 
+
+smallest_3()

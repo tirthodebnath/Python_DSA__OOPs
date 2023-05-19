2480ffb1017b6f4cd63231b93e0ea031b3a96577
TIRTHANKAR DEBNATH <55769445+tirthodebnath@users.noreply.github.com>
2023-05-19 11:33:04 +0530

    changes

--- DSA - Udemy/list/test.py
+++ DSA - Udemy/list/test.py
@@ -1,2 +1,52 @@
-##write a recustion in python 
+##Python Program to Print Largest Even and Largest Odd Number in a List 
 
+
+# def new_func():
+#     def find_largest_even_odd(numbers):
+#     # Initialize variables to store the largest even and odd numbers
+#         largest_even = 0
+#         largest_odd = 0
+
+#     # Iterate through the numbers
+#         for num in numbers:
+#         # Check if the current number is even
+#             if num % 2 == 0:
+#             # Update the largest even number if necessary
+#                 if num > largest_even:
+#                     largest_even = num
+#             else:
+#             # Update the largest odd number if necessary
+#                 if num > largest_odd:
+#                     largest_odd = num
+
+#     # Print the largest even and odd numbers
+#         print("Largest even number:", largest_even)
+#         print("Largest odd number:", largest_odd)
+#         print(type(largest_even))
+
+
+# # Test the function
+#     num_list = [10, 5, 7, 2, 8, 15]
+#     find_largest_even_odd(num_list)
+
+# new_func()
+
+
+######################################################################
+##Common elements between 2 lists
+
+list1 = [1, 2, 3, 4, 5,7]
+list2 = [4, 5, 6, 7, 8]
+
+common_elements = []
+
+for i in list1:
+    if i in list2:
+        common_elements.append(i)
+print(common_elements)
+
+######################################################################
+
+
+
+            

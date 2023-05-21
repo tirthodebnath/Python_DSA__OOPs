09c12bfa0dd58ca1dc7a5d5df492aa807d46e56a
TIRTHANKAR DEBNATH <55769445+tirthodebnath@users.noreply.github.com>
2023-05-22 02:23:49 +0530

    changes

--- SQL/test.sql
+++ SQL/test.sql
@@ -101,3 +101,14 @@ Delete p1
 from Person p1, Person p2
 where p1.Email = p2.Email and p1.Id > p2.Id
 
+
+
+/*Inner join using asc*/
+
+select first_name , item , amount 
+from customers 
+outer join orders 
+on customers.customer_id = orders.customer_id 
+order by amount asc
+
+

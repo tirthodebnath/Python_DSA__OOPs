f94761b189dd3bf8f841ece8602f926b9855bfdd
TIRTHANKAR DEBNATH <55769445+tirthodebnath@users.noreply.github.com>
2023-06-01 20:53:27 +0530

    changes

--- Test/read_txt.py
+++ Test/read_txt.py
@@ -1,11 +1,52 @@
-# Open the text file in read mode
-file = open("Tricks_in_Python.txt", "r")
+# # Open the text file in read mode
+# file = open("Tricks_in_Python.txt", "r")
 
-# Read the entire content of the file
-content = file.read()
+# # Read the entire content of the file
+# content = file.read()
 
-# Print the content
-print(content)
+# # Print the content
+# print(content)
 
-# Close the file
-file.close()
+# # Close the file
+# file.close()
+
+
+
+# def solve(a):
+#     a=[1,3,5]
+
+# a=[2,4,6]
+
+# print(a)
+# solve(a)
+# print(a)
+
+# a=[1,2,3]
+
+# a=tuple(a)
+
+# a[0]=2
+
+# print(a)
+
+# count=0
+
+# while(True):
+#     if count % 3==0:
+#         print(count, end="")
+#     if(count> 15):
+#         break
+#     count+=1
+
+
+# input=["2","4","a","6"]
+# result=[[i]+[i]for i in input if i]
+# print(result)
+
+# def is_even(number):
+#     message= f"{number} is an even number" if number % 2 ==0 else f"{number} is a na odd number"
+#     return message
+
+# print(is_even(54))
+a="hello"
+print(sorted(set(a)))

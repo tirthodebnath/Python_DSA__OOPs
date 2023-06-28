59a96697cedabc951c800a9b1abe46679430d192
TIRTHANKAR DEBNATH <55769445+tirthodebnath@users.noreply.github.com>
2023-06-28 12:48:36 +0530

    tets

--- Test/read_txt.py
+++ Test/read_txt.py
@@ -55,16 +55,16 @@
 ###################################################################
 
 ##Largest uiq number 
-lst = [8, 8, 3, 3, 1, 4, 5, 6]
-non_repeated = []
+# lst = [8, 8, 3, 3, 1, 4, 5, 6]
+# non_repeated = []
 
-for num in lst:
-    if lst.count(num) == 1:
-        non_repeated.append(num)
+# for num in lst:
+#     if lst.count(num) == 1:
+#         non_repeated.append(num)
 
-print(max(non_repeated))
+# print(max(non_repeated))
 
-print("Non-repeated numbers:", non_repeated)
+# print("Non-repeated numbers:", non_repeated)
 
 
 

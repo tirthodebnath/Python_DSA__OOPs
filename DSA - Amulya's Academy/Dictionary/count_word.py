609c1bb77efb62e153a89796c35ccfc6efaf39b6
TIRTHANKAR DEBNATH <55769445+tirthodebnath@users.noreply.github.com>
2024-05-17 12:04:03 +0530

    test

--- DSA - Amulya's Academy/Dictionary/count_word.py
+++ DSA - Amulya's Academy/Dictionary/count_word.py
@@ -1,5 +1,8 @@
 ## Count uniq letters of a word
 
 word = "tirtho"
-char_dict = {char: word.count(char) for char in word}
-print(char_dict)
+# char_dict = {char: word.count(char) for char in word}
+# print(char_dict)
+
+for i in word:
+    print(word.count(i))
